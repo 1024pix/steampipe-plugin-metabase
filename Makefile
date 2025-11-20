798fab3ccc0af5b0ed8f6a7509b0cc596db64b8d
Romain (Cyber @ Pix) <romain.du-marais+github@pix.fr>
2025-11-20 21:00:08 +0100

    feat: added 1 table for metabase users

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 install:
 	go build -o  ~/.steampipe/plugins/hub.steampipe.io/plugins/1024pix/metabase@latest/steampipe-plugin-metabase.plugin *.go
+
+dev:
+	go build -o  ~/.steampipe/plugins/local/metabase@latest/steampipe-plugin-metabase.plugin *.go
--- metabase/table_metabase_user.go
+++ metabase/table_metabase_user.go
@@ -8,12 +8,12 @@ import (
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin/transform"
 )
 
-func tableMetabaseGroup() *plugin.Table {
+func tableMetabaseUser() *plugin.Table {
 	return &plugin.Table{
 		Name:        "metabase_user",
 		Description: "List of users of metabase.",
 		List: &plugin.ListConfig{
-			Hydrate: listUser,
+			Hydrate: listUsers,
 		},
 		Get: &plugin.GetConfig{
 			KeyColumns: plugin.AllColumns([]string{"id"}),
@@ -22,38 +22,38 @@ func tableMetabaseGroup() *plugin.Table {
 		Columns: []*plugin.Column{
 			// Key column cannot be a pointer. Transform helps us to manage them
 			{Name: "id", 			Type: proto.ColumnType_INT, 	Transform: transform.FromField("Id"), 	Description: "ID of the user."},
-			{Name: "email", 		Type: proto.ColumnType_STRING, 	Transform: transform.FromField("Id"), 	Description: "User login email."},
-			{Name: "common_name", 	Type: proto.ColumnType_STRING, 	Transform: transform.FromField("Id"), 	Description: "User common name."},
-			{Name: "first_name", 	Type: proto.ColumnType_STRING, 	Transform: transform.FromField("Id"), 	Description: "User first name."},
-			{Name: "last_name", 	Type: proto.ColumnType_STRING, 	Transform: transform.FromField("Id"), 	Description: "User last name."},
-			{Name: "locale", 		Type: proto.ColumnType_STRING, 	Transform: transform.FromField("Id"), 	Description: "Locale string chosen by user."},
-			{Name: "last_login", 	Type: proto.ColumnType_TIMESTAMP, Transform: transform.FromField("Id"), Description: "Date and time of user last login."},
-			{Name: "is_active", 	Type: proto.ColumnType_BOOL, 	Transform: transform.FromField("Id"), 	Description: "Is user activated by an admin."},
-			{Name: "updated_at", 	Type: proto.ColumnType_TIMESTAMP, Transform: transform.FromField("Id"), Description: "Date and time of last change in user profile."},
-			{Name: "date_joined", 	Type: proto.ColumnType_TIMESTAMP, Transform: transform.FromField("Id"), Description: "Date and time of user creation (or first connexion ?)."},
-			{Name: "group_ids", Type: proto.ColumnType_STRING, 		Transform: transform.FromField("Id"), 	Description: "Array of group IDs (type incorrect !)."},
-			{Name: "is_superuser", 	Type: proto.ColumnType_BOOL, 	Transform: transform.FromField("Id"), 	Description: "True if user id admin."},
-			{Name: "personal_collection_id", Type: proto.ColumnType_INT, Transform: transform.FromField("Id"), Description: "Link with user collection."},
-			{Name: "is_qbnewb", 	Type: proto.ColumnType_BOOL, 	Transform: transform.FromField("Id"), 	Description: "Unknown usage."},
-			{Name: "jwt_attributes", Type: proto.ColumnType_STRING, Transform: transform.FromField("Id"), 	Description: "Unknown usage and type."},
+			{Name: "email", 		Type: proto.ColumnType_STRING, 	                                     	Description: "User login email."},
+			{Name: "common_name", 	Type: proto.ColumnType_STRING, 	                                     	Description: "User common name."},
+			{Name: "first_name", 	Type: proto.ColumnType_STRING, 	                                     	Description: "User first name."},
+			{Name: "last_name", 	Type: proto.ColumnType_STRING, 	                                     	Description: "User last name."},
+			{Name: "locale", 		Type: proto.ColumnType_STRING, 	                                     	Description: "Locale string chosen by user."},
+			{Name: "last_login", 	Type: proto.ColumnType_TIMESTAMP, 										Description: "Date and time of user last login."},
+			{Name: "is_active", 	Type: proto.ColumnType_BOOL, 										 	Description: "Is user activated by an admin."},
+			{Name: "updated_at", 	Type: proto.ColumnType_TIMESTAMP, 										Description: "Date and time of last change in user profile."},
+			{Name: "date_joined", 	Type: proto.ColumnType_TIMESTAMP, 										Description: "Date and time of user creation (or first connexion ?)."},
+			{Name: "group_ids", Type: proto.ColumnType_STRING, 											 	Description: "Array of group IDs (type incorrect !)."},
+			{Name: "is_superuser", 	Type: proto.ColumnType_BOOL, 										 	Description: "True if user id admin."},
+			{Name: "personal_collection_id", Type: proto.ColumnType_INT, 									Description: "Link with user collection."},
+			{Name: "is_qbnewb", 	Type: proto.ColumnType_BOOL, 										 	Description: "Unknown usage."},
+			{Name: "jwt_attributes", Type: proto.ColumnType_STRING, 									 	Description: "Unknown usage and type."},
 			// {Name: "login_attributes", Type: proto.ColumnType_INT, Transform: transform.FromField("Id"), Description: "ID of the group."},
-			{Name: "tenant_id", 	Type: proto.ColumnType_STRING, 	Transform: transform.FromField("Id"), 	Description: "Unknown usage and type."},
-			{Name: "sso_source", 	Type: proto.ColumnType_STRING, 	Transform: transform.FromField("Id"), 	Description: "Name of SSO if uses an SSO."},
+			{Name: "tenant_id", 	Type: proto.ColumnType_STRING, 										 	Description: "Unknown usage and type."},
+			{Name: "sso_source", 	Type: proto.ColumnType_STRING, 										 	Description: "Name of SSO if uses an SSO."},
 		},
 	}
 }
 
-func listUser(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData) (interface{}, error) {
+func listUsers(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData) (interface{}, error) {
 	client, err := connect(d)
 
 	if err != nil {
-		plugin.Logger(ctx).Error("metabase_user.listUser", "connection_error", err)
+		plugin.Logger(ctx).Error("metabase_user.listUsers", "connection_error", err)
 		return nil, err
 	}
 
-	users, resp, err := client.GetUsers(context.Background())
+	users, resp, err := client.ListUsers(context.Background())
 
-	err = manageError("metabase_user.listUser", ctx, resp, err)
+	err = manageError("metabase_user.listUsers", ctx, resp, err)
 
 	if err != nil {
 		return nil, err
